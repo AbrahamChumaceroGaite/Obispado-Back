9e4a98cd0fccfe36e79638b25f61bc326c6b59fd
AbrahamChumaceroGaite <abraham.chumacero@ucb.edu.bo>
2023-12-01 10:46:10 -0400

    big

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const verifyToken = require('./middleware/middleware'); // Importar el middleware verifyToken
@@ -29,17 +30,20 @@ app.use("/api/Token", l_login);
 /* app.use(verifyToken); */
 
 // Rutas protegidas que requieren token
-app.use("/api/parish",  r_parish);
+app.use("/api/parish", r_parish);
 app.use("/api/user", r_user);
-app.use("/api/signatory",  r_signatory);
+app.use("/api/signatory", r_signatory);
 app.use("/api/certificate", r_certificate);
 
 // Manejador de errores
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Error del servidor' });
-  });
+  console.error(err.stack);
+  res.status(500).json({ error: 'Error del servidor' });
+});
 
-app.listen(80, () => {
-  console.log(`Servidor Prendido`);
+const port = 80; // Puerto obtenido de las variables de entorno
+
+http.listen(port, function () {
+  console.log('\n');
+  console.log(`>> Express listo y escuchando por el puerto ` + port);
 });
